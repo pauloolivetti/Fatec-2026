c9f760ce09a9bca49d4451cee0da656b89476cf7
pauloolivetti <paulo.olivetti21@gmail.com>
2026-03-26 00:58:29 -0300

    Atividade 2 finalizada

--- Desenvolvimento Web 1ºSem/Desenvolvimento Web atv-2/public/assets/js/main.js
+++ Desenvolvimento Web 1ºSem/Desenvolvimento Web atv-2/public/assets/js/main.js
@@ -1,32 +1,52 @@
 const input = document.getElementById("search-input");
 const result = document.getElementById("result");
 
-function showresult(message){
-    result.textContent = message;
+function showresult(message, type){
+    result.innerHTML = message;
+    result.className = `result ${type}`;
 }
 
-function handlecepSearch(){
-    const rawCep = input.value.replace(/\D/g,""); 
-}
+async function handlecepSearch() {
+    const rawCep = input.value.replace(/\D/g, "");
 
-    if( rawCep.length != 8 ){
-        showresult("CEP inválido. Use 8 números (ex: 12345678 ou 12.328-070).");
-    
-    } 
+    if (rawCep.length !== 8) {
+        showresult("CEP inválido. Use 8 números (ex: 12345678 ou 12.328-070).", "error");
+     } else{                            
+        showresult("consultando cep...", "loading");   
+        const response = await fetch(`https://viacep.com.br/ws/${rawCep}/json/`);
+        if ( !response.ok ) {
+            showresult("falha no serviço", "error");
+          } else{
+            const data = await response.json();
+            if (data.erro) {
+                showresult("CEP não encontrado.", "error");
+          } else {
+            const message = `
+            <p><strong>CEP:</strong> ${data.cep  || 'N/A'}</p>
+            <p><strong>Logradouro:</strong> ${data.logradouro || 'N/A'}</p>
+            <p><strong>Complemento:</strong> ${data.complemento || 'N/A'}</p>
+            <p><strong>Bairro:</strong> ${data.bairro || 'N/A'}</p>
+            <p><strong>Cidade:</strong> ${data.localidade || 'N/A'}</p>
+            <p><strong>UF:</strong> ${data.uf || 'N/A'}</p>
+            <p><strong>Estado:</strong> ${data.estado || 'N/A'}</p>
+            <p><strong>Região:</strong> ${data.regiao || 'N/A'}</p>
+            <p><strong>DDD:</strong> ${data.ddd || 'N/A'}</p>`;    
 
-
-
-
-input.addEventListener("keydown",function(event) {
+            showresult(message, "success")   
+           }
+        }
+        }
+    };
+     
+input.addEventListener("keydown",function(event){
     if (event.key == "Enter" ) {
         handlecepSearch();
     }
+});
+
+input.addEventListener("input", function(){
+    if( input.value.trim() == "" ){
+        result.innerHTML = "";
+        result.className = "result";
+    }
 })
-
-
-
-
-
-
-
-
